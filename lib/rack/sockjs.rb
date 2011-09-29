0871d60dbe10bb8194750e24ef7482e016298676
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-09-29 13:08:33 +0100

    OK, let's move SockJS.start(&block) to the middleware for now.

--- lib/rack/sockjs.rb
+++ lib/rack/sockjs.rb
@@ -10,15 +10,19 @@ require "sockjs"
 #   run MyApp
 module Rack
   class SockJS
-    def initialize(app, connection, prefix = "/echo")
-      @app, @prefix, @connection = app, connection, prefix
+    def initialize(app, prefix = "/echo")
+      @app, @prefix = app, prefix
     end
 
     def call(env)
-      if env["PATH_INFO"].match(/^#{PREFIX}\//)
-        prefix  = env["PATH_INFO"].split("/")[2]
-        handler = @connection.handler(prefix.to_sym)
-        handler.handle(env)
+      if env["PATH_INFO"].match(/^#@prefix\//)
+        ::SockJS.start do |connection|
+          prefix  = env["PATH_INFO"].split("/")[2]
+          handler = connection.handler(prefix.to_sym)
+          handler.handle(env)
+
+          EM.stop
+        end
       else
         @app.call(env)
       end
