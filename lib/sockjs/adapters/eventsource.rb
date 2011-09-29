f54bbaa8bec85f721c6e9e87f84de7d7c9d00500
botanicus <stastny@101ideas.cz>
2011-09-29 17:15:32 +0100

    EventSource adapter.

--- lib/sockjs/adapters/eventsource.rb
+++ lib/sockjs/adapters/eventsource.rb
@@ -12,7 +12,18 @@ module SockJS
 
       # Handler.
       def self.handle(env)
-        raise NotImplementedError.new
+        # session = transport.Session.bySessionIdOrNew(req.session, req.sockjs_server)
+        # session.register( new EventSourceReceiver(res, req.sockjs_server.options) )
+
+        # Opera needs to hear two more initial new lines.
+        body = "\r\n\r\n"
+        [200, {"Content-Type" => "text/event-stream"}, [body]]
+      end
+
+      def self.send_frame(payload)
+        # Beware of leading whitespace
+        data = ["data: ", escape_selected(payload, "\r\n\x00"), "\r\n\r\n"]
+        super(data.join)
       end
     end
   end
