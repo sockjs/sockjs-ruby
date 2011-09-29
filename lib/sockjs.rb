58ff7d5646f0b08c560ad1541a36896be6d17c45
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-09-29 11:49:28 +0100

    SockJS.connect(options = Hash.new) and SockJS::Connection module.

---
+++ lib/sockjs.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+require "eventmachine"
+
+module SockJS
+  def self.connect(options = Hash.new)
+    host = options[:host] || "127.0.0.1"
+    port = options[:port] || 9999
+    EventMachine.connect(host, port, Connection, options)
+  end
+
+  def self.start(options = Hash.new, &block)
+    EM.run do
+      block.call(self.connect(options))
+    end
+  end
+
+  module Connection
+    def post_init
+    end
+
+    def receive_data(data)
+    end
+  end
+end
---
+++ spec/sockjs_spec.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+require "spec_helper"
+require "sockjs"
+
+describe SockJS do
+  describe ".connect" do
+    it "should work without any arguments" do
+      EM.run do
+        -> { described_class.connect }.should_not raise_error
+
+        EM.stop
+      end
+    end
+
+    it "should let you specify options such as port and host" do
+      EM.run do
+        -> { described_class.connect(host: "localhost", port: 9292) }.should_not raise_error
+
+        EM.stop
+      end
+    end
+
+    it "should include SockJS::Connection" do
+      EM.run do
+        connection = described_class.connect
+        meta_class = class << connection; self; end
+        meta_class.included_modules.should include(described_class::Connection)
+
+        EM.stop
+      end
+    end
+  end
+end
