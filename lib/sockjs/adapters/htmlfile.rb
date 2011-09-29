935ae04ec9082accab99a60c1365873ce1eddfbe
botanicus <stastny@101ideas.cz>
2011-09-29 17:02:44 +0100

    Bloody hell, Ruby went crazy, where the hell are my DATA?!

--- lib/sockjs/adapters/htmlfile.rb
+++ lib/sockjs/adapters/htmlfile.rb
@@ -20,10 +20,19 @@ module SockJS
         callback = qs["c"] || qs["callback"]
 
         if callback
+          # What the fuck is wrong with Ruby???
+          # The bloody pseudoconstant ::DATA is supposed
+          # to be avaible anywhere where we have __END__!
+          data = begin
+            lines = File.readlines(__FILE__)
+            index = lines.index("__END__\n")
+            lines[(index + 1)..-1].join("")
+          end
+
           # Safari needs at least 1024 bytes to parse the website. Relevant:
           #   http://code.google.com/p/browsersec/wiki/Part2#Survey_of_content_sniffing_behaviors
-          html = ::DATA.gsub("{{ callback }}", callback)
-          body = html + (" " * (1024 - ::DATA.bytesize)) + "\r\n\r\n"
+          html = data.gsub("{{ callback }}", callback)
+          body = html + (" " * (1024 - html.bytesize)) + "\r\n\r\n"
           [200, {"Content-Type" => "text/html", "Content-Length" => body.bytesize.to_s}, [body]]
 
           # TODO:
