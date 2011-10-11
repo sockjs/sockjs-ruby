98a690086bbd17566c8116959d4d1f458b9041ce
botanicus <stastny@101ideas.cz>
2011-10-11 11:22:28 +0100

    Chunking test basically works

--- lib/sockjs/utils.rb
+++ lib/sockjs/utils.rb
@@ -7,11 +7,23 @@ module SockJS
       @hash = hash
     end
 
+    # def each(&block)
+    #   wait_for = @hash.reduce(0) do |wait_for, (ms, data)|
+    #     EM.add_timer(ms / 1000.0) do
+    #       block.call(data)
+    #     end
+    #
+    #     wait_for + ms
+    #   end
+    #
+    #   sleep (wait_for + 500) / 1000.0 # TODO: How to do it in a non-blocking fashion?
+    # end
+
+    # TODO: Figure out how to make it EM-compatible (see above).
     def each(&block)
       @hash.each do |ms, data|
-        EM.add_timer(ms / 1000.0) do
-          block.call(data)
-        end
+        sleep ms / 1000.0
+        block.call(data)
       end
     end
   end
