2fb0101ec9c7cb80a88eb1d20b6b8229472c7538
botanicus <stastny@101ideas.cz>
2011-10-24 12:34:36 +0100

    On the way to working XHR transport. Uff, we need to implement some sessions.

--- lib/sockjs/adapters/xhr.rb
+++ lib/sockjs/adapters/xhr.rb
@@ -6,55 +6,63 @@ module SockJS
   module Adapters
     class XHRPost < Adapter
       # Settings.
-      self.prefix  = "xhr"
+      self.prefix  = /[^.]+\/([^.]+)\/xhr$/
       self.method  = "POST"
       self.filters = [:h_sid, :xhr_cors, :xhr_poll]
 
       # Handler.
       def self.handle(env, options)
-        raise NotImplementedError.new
+        match = env["PATH_INFO"].match(self.prefix)
+        data  = self.sessions[match[1]]
+        [200, {"Content-Type" => "text/plain", "Content-Length" => "2"}, ["o\n"]]
       end
     end
 
     class XHROptions < Adapter
       # Settings.
-      self.prefix  = "xhr"
+      self.prefix  = /[^.]+\/([^.]+)\/xhr$/
       self.method  = "OPTIONS"
       self.filters = [:h_sid, :xhr_cors, :cache_for, :xhr_options, :expose]
 
       # Handler.
       def self.handle(env, options)
+        match = env["PATH_INFO"].match(self.prefix)
+        p session_id = match[1]
         raise NotImplementedError.new
       end
     end
 
     class XHRSendPost < Adapter
       # Settings.
-      self.prefix  = "xhr_send"
+      self.prefix  = /[^.]+\/([^.]+)\/xhr_send$/
       self.method  = "POST"
       self.filters = [:h_sid, :xhr_cors, :expect_xhr, :xhr_send]
 
       # Handler.
       def self.handle(env, options)
-        raise NotImplementedError.new
+        match = env["PATH_INFO"].match(self.prefix)
+        p session_id = match[1]
+        [204, {}, Array.new]
       end
     end
 
     class XHRSendOptions < Adapter
       # Settings.
-      self.prefix  = "xhr_streaming"
+      self.prefix  = /[^.]+\/([^.]+)\/xhr_send$/
       self.method  = "OPTIONS"
       self.filters = [:h_sid, :xhr_cors, :cache_for, :xhr_options, :expose]
 
       # Handler.
       def self.handle(env, options)
+        match = env["PATH_INFO"].match(self.prefix)
+        p session_id = match[1]
         raise NotImplementedError.new
       end
     end
 
     class XHRStreamingPost < Adapter
       # Settings.
-      self.prefix  = "xhr_streaming"
+      self.prefix  = /[^.]+\/([^.]+)\/xhr_streaming$/
       self.method  = "POST"
       self.filters = [:h_sid, :xhr_cors, :xhr_streaming]
 
@@ -66,7 +74,7 @@ module SockJS
 
     class XHRStreamingOptions < Adapter
       # Settings.
-      self.prefix  = "xhr_send"
+      self.prefix  = /[^.]+\/([^.]+)\/xhr_streaming$/
       self.method  = "OPTIONS"
       self.filters = [:h_sid, :xhr_cors, :cache_for, :xhr_options, :expose]
 
