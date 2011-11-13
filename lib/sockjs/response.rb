e20f87529847d97025e95cf67c3af6a125d22bbf
botanicus <stastny@101ideas.cz>
2011-11-13 08:30:06 +0000

    SockJS::Response is a compatibility layer, so we can support Rack, Goliath and others.

---
+++ lib/sockjs/response.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+module SockJS
+  # This class is a compatibility layer which makes it possible
+  # to work with Rack as well as with other HTTP libraries, Goliath etc.
+  # This class is not supposed to be instantiated directly, you have to
+  # subclass it and rewrite some library-dependent methods.
+
+  # The API is heavily inspired by Node.js' standard library.
+  class Response
+    attr_reader :headers
+    def initialize
+      @headers, @body = Hash.new, String.new
+    end
+
+    def set_header(key, value)
+      @headers[key] = value
+    end
+
+    def write_head
+      raise NotImplementedError.new("This is supposed to be rewritten in subclasses!")
+    end
+
+    def write(data)
+      raise NotImplementedError.new("This is supposed to be rewritten in subclasses!")
+    end
+  end
+end
