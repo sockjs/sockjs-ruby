0f33a7a35a0ec748286188343a4f3a5ea96c9a08
botanicus <stastny@101ideas.cz>
2011-11-13 10:13:28 +0000

    Basic RackResponse. It will be probably more complicated in the future.

---
+++ lib/sockjs/adapters/servers/rack.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+require "sockjs/response"
+
+module SockJS
+  class RackResponse < Response
+    def write_head(status = nil, headers = nil)
+      @status  = status  || @status
+      @headers = headers || @headers
+    end
+
+    def write(data)
+      @body << data
+    end
+
+    def finish(data = nil)
+      self.write(data) if data
+
+      [@status, @headers, @body]
+    end
+  end
+end
