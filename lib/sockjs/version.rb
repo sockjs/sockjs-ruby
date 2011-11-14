6b61efb019b50cae983af758d1a7f6d30b387dc3
botanicus <stastny@101ideas.cz>
2011-11-14 20:02:39 +0000

    Let's introduce SockJS::PROTOCOL_VERSION.

--- lib/sockjs/version.rb
+++ lib/sockjs/version.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 
 module SockJS
-  unless defined?(VERSION)
-    VERSION = "0.0.1"
-  end
+  # PROTOCOL_VERSION is SockJS protocol version used,
+  # whereas VERSION is version of the gem. They are
+  # currently the same, however this is NOT guaranteed
+  # to be true in the future!
+  PROTOCOL_VERSION ||= VERSION ||= "0.1"
 end
