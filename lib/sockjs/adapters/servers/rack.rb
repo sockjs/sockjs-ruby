4398c1a69c0310625052655e4a8d1eb152a980c6
botanicus <stastny@101ideas.cz>
2011-11-15 12:11:18 +0000

    Let's move every Rack* into Rack module.

--- lib/sockjs/adapters/servers/rack.rb
+++ lib/sockjs/adapters/servers/rack.rb
@@ -1,41 +1,47 @@
 # encoding: utf-8
 
+require "sockjs/request"
 require "sockjs/response"
 
 module SockJS
-  class RackResponse < Response
-    def write_head(status = nil, headers = nil)
-      super(status, headers) do
-        # The actual implementation.
+  module Rack
+    class Request < Request
+    end
+
+    class Response < Response
+      def write_head(status = nil, headers = nil)
+        super(status, headers) do
+          # The actual implementation.
+        end
+      end
+
+      def write(data)
+        super() do
+          @body << data
+        end
+      end
+
+      def finish(data = nil)
+        super(data) do
+          [@status, @headers, [@body]]
+        end
       end
     end
 
-    def write(data)
-      super() do
-        @body << data
+    class DelayedResponseBody
+      # Implementation: fibers? EM?
+
+      # response.write("data")
+      def each(&block)
+        # wait until finish
+        # block.call(data) if block
       end
-    end
 
-    def finish(data = nil)
-      super(data) do
-        [@status, @headers, [@body]]
+      # this refactoring means we'll return [200, {}, []] in first write, not finish!
+
+      def finish
+        # done
       end
-    end
-  end
-
-  class RackDelayedResponseBody
-    # Implementation: fibers? EM?
-
-    # response.write("data")
-    def each(&block)
-      # wait until finish
-      # block.call(data) if block
-    end
-
-    # this refactoring means we'll return [200, {}, []] in first write, not finish!
-
-    def finish
-      # done
     end
   end
 end
