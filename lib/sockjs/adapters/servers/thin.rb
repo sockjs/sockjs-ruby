45c6efde921f1e2c2caa295fd82a12c6dea42eb0
botanicus <stastny@101ideas.cz>
2011-11-23 14:03:39 +0000

    Thin::AsyncResponse implemented.

--- lib/sockjs/adapters/servers/thin.rb
+++ lib/sockjs/adapters/servers/thin.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 
+require "forwardable"
+
 require_relative "./rack"
 
 module SockJS
@@ -7,7 +9,31 @@ module SockJS
     class Request < Rack::Request
     end
 
+
     # This is just to make Rack happy.
     DUMMY_RESPONSE ||= [-1, Hash.new, Array.new]
+
+
+    class AsyncResponse < Response
+      extend Forwardable
+
+      # env["async.callback"]
+      def initialize(async_callback, status = nil, headers = Hash.new, &block)
+        @async_callback   = async_callback
+        @status, @headers = status, headers
+        @body = DelayedResponseBody.new
+
+        block.call(self) if block
+      end
+
+      def write_head(status = nil, headers = nil)
+        super(status, headers) do
+          @async_callback.call(@status, @headers, @body)
+        end
+      end
+
+      def_delegator :body, :write
+      def_delegator :body, :finish
+    end
   end
 end
