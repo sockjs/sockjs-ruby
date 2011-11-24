50601f1d5be131cbf498d62d6a611ff6010e6c00
botanicus <stastny@101ideas.cz>
2011-11-24 12:55:03 +0000

    Let's remove this old piece of synchronous crap, time to redo it has come!

--- lib/sockjs/utils.rb
+++ lib/sockjs/utils.rb
@@ -18,24 +18,5 @@ module SockJS
     #
     #   sleep (wait_for + 500) / 1000.0 # TODO: How to do it in a non-blocking fashion?
     # end
-
-    # TODO: Figure out how to make it EM-compatible (see above).
-    def each(&block)
-      @hash.each.reduce(0) do |already_waited, (execution_time_in_ms, data)|
-        seconds_per_iteration = timer do
-          execution_time = execution_time_in_ms / 1000.0
-
-          sleep execution_time - already_waited
-          puts block.call(data)
-        end
-
-        already_waited + seconds_per_iteration
-      end
-    end
-
-    protected
-    def timer(&block)
-      - (Time.now.to_f.tap(&block) - Time.now.to_f)
-    end
   end
 end
