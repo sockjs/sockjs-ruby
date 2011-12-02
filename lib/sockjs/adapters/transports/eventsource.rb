7d9a8dfa32bf5ee732cf590f54830e2807440f32
botanicus <stastny@101ideas.cz>
2011-12-02 12:02:43 +0000

    EventSource transport works.

--- lib/sockjs/adapters/transports/eventsource.rb
+++ lib/sockjs/adapters/transports/eventsource.rb
@@ -12,18 +12,39 @@ module SockJS
 
       # Handler.
       def handle(request)
-        # session = transport.Session.bySessionIdOrNew(req.session, req.sockjs_server)
-        # session.register( new EventSourceReceiver(res, req.sockjs_server.options) )
+        response = self.response(request, 200, {"Content-Type" => CONTENT_TYPES[:event_stream], "Cache-Control" => "no-store, no-cache, must-revalidate, max-age=0"})  { |response| response.set_session_id(request.session_id) }
+        response.write_head
 
         # Opera needs to hear two more initial new lines.
-        body = "\r\n\r\n"
-        self.write_response(request, 200, {"Content-Type" => CONTENT_TYPES[:event_stream], "Cache-Control" => "no-store, no-cache, must-revalidate, max-age=0"}, body)  { |response| response.set_session_id(request.session_id) }
+        response.write("\r\n")
+
+        match = request.path_info.match(self.class.prefix)
+
+        unless session = self.connection.sessions[match[1]]
+          session = self.connection.create_session(match[1])
+          body = self.format_frame(session.open!.chomp)
+          response.write(body)
+        end
+
+        EM::PeriodicTimer.new(1) do |timer|
+          if data = session.process_buffer
+            response.write(format_frame(data.chomp!)) unless data == "a[]\n" # FIXME
+            if data[0] == "c" # close frame. TODO: Do this by raising an exception or something, this is a mess :o Actually ... do we need here some 5s timeout as well?
+              timer.cancel
+              response.finish
+            end
+          end
+        end
       end
 
-      def send_frame(payload)
+      def format_frame(payload)
         # Beware of leading whitespace
-        data = ["data: ", escape_selected(payload, "\r\n\x00"), "\r\n\r\n"]
-        super(data.join)
+        ["data: ", payload, "\r\n\r\n"].join
+        # ["data: ", escape_selected(payload, "\r\n\x00"), "\r\n\r\n"].join
+      end
+
+      def escape_selected(*args)
+        args.join
       end
     end
   end
