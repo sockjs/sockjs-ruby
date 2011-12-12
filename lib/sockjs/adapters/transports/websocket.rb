8314505e118e74410fdb6193def82e65d127beb9
botanicus <stastny@101ideas.cz>
2011-12-12 13:26:29 +0000

    OK, WebSocket transport can look like that ...

--- lib/sockjs/adapters/transports/websocket.rb
+++ lib/sockjs/adapters/transports/websocket.rb
@@ -5,7 +5,50 @@ require "sockjs/adapter"
 module SockJS
   module Adapters
     class WebSocket < Adapter
+      # Settings.
+      self.prefix  = /[^.]+\/([^.]+)\/websocket$/
+      self.method  = "GET"
+
+      def invalid_request_or_disabled_websocket?(request)
+        if self.disabled?
+          status, body = 404, "WebSockets Are Disabled"
+        elsif request.env["HTTP_CONNECTION"] != "Upgrade"
+          status, body = 400, '"Connection" must be "Upgrade".'
+        elsif request.env["HTTP_UPGRADE"] != "WebSocket"
+          status, body = 400, 'Can "Upgrade" only to "WebSocket".'
+        else
+          return false
+        end
+
+        p [status, {"Content-Length" => body.bytesize.to_s}, [body]]
+        [status, {"Content-Length" => body.bytesize.to_s}, [body]]
+      end
+
       # Handlers.
+      def handle(request)
+        unless invalid_request_or_disabled_websocket?(request)
+          puts "~ Upgrading to WebSockets ..."
+
+          ws = Faye::WebSocket.new(env)
+          handler = ::SockJS::Adapters::WebSocket.new(@connection, @options)
+
+          handler.handle_open(request, ws)
+
+          ws.onmessage = lambda do |event|
+            debug "~ WS data received: #{event.data.inspect}"
+            handler.handle_message(request, event, ws)
+          end
+
+          ws.onclose = lambda do |event|
+            debug "~ Closing WebSocket connection (#{event.code}, #{event.reason})"
+            handler.handle_close(request, ws)
+          end
+
+          # Thin async response
+          ::SockJS::Thin::DUMMY_RESPONSE
+        end
+      end
+
       def handle_open(request, ws)
         p [:ws_open]
         ws.send("o")
