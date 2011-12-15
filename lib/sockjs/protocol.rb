a2f9394998c7ddbb288b22b0845c0ca16ca67929
botanicus <stastny@101ideas.cz>
2011-12-15 11:13:18 +0000

    Protocol refactoring.

--- lib/sockjs/protocol.rb
+++ lib/sockjs/protocol.rb
@@ -1,22 +1,29 @@
 # encoding: utf-8
 
+require "json"
+
 module SockJS
   class Protocol
-    OPEN_FRAME  ||= "o"
-    CLOSE_FRAME ||= "c"
-    ARRAY_FRAME ||= "a"
-
-    HEARTBEAT_FRAME ||= "h\n"
+    OPEN_FRAME      ||= "o"
+    CLOSE_FRAME     ||= "c"
+    ARRAY_FRAME     ||= "a"
+    HEARTBEAT_FRAME ||= "h"
 
     def self.array_frame(array)
-      "#{ARRAY_FRAME}#{array.to_json}\n"
+      "#{ARRAY_FRAME}#{array.to_json}"
     end
 
     def self.close_frame(status, message)
-      if status && message
-        "#{CLOSE_FRAME}[#{status},#{message.inspect}]\n"
-      else
-        CLOSE_FRAME + "\n"
+      validate Integer, status
+      validate String, message
+
+      "#{CLOSE_FRAME}[#{status},#{message.inspect}]"
+    end
+
+    protected
+    def validate(desired_class, object)
+      unless object.is_a?(desired_class)
+        raise ArgumentError.new("#{desired_class} object expected, but object is an instance of #{object.class} (object: #{object.inspect}).")
       end
     end
   end
