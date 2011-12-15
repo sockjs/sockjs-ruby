c72d2cb3af16846a824330c7b204136994e64d1a
botanicus <stastny@101ideas.cz>
2011-12-15 11:31:07 +0000

    Documentation for SockJS::Buffer, more methods, sanity checks.

--- lib/sockjs/buffer.rb
+++ lib/sockjs/buffer.rb
@@ -3,23 +3,53 @@
 require "sockjs/protocol"
 
 class SockJS
-  class BufferClosedError < StandardError
+  class BufferNotOpenError < StandardError
+  end
+
+  class StandardError < StandardError
+    def initialize(actual_status, aim_status)
+      super("Can't change from #{actual_status} to #{aim_status}!")
+    end
   end
 
   class Buffer
     def initialize
-      @messages = Array.new
+      @status, @messages = :created, Array.new
     end
 
+    # Open frame has to be the first frame.
+    def open
+      if @status == :created
+        @status = :opened
+        @frame  = Protocol::OPEN_FRAME
+      else
+        raise StateMachineError.new(@status, :opened)
+      end
+    end
+
+    # Close frame can occur at any time, except if the session isn't open yet.
+    # Also, if the buffer is already closed, let's fail: I believe this is more transparent behaviour.
     def close(*args)
-      @frame = Protocol.close_frame(*args)
+      unless @status == :created or @status == :closed
+        @status = :closed
+        @frame  = Protocol.close_frame(*args)
+      else
+        raise StateMachineError.new(@status, :closed)
+      end
     end
 
+    # Add message to the list of messages.
     def <<(message)
-      raise BufferClosedError.new if @frame
+      raise BufferNotOpenError.new if @frame
       @messages << message
     end
 
+    # In case you need to rewrite content of the buffer, you can do so calling #clear.
+    def clear
+      @frame = nil; @messages.clear
+    end
+
+    # To get the data encoded as a SockJS frame.
     def to_frame
       @frame || Protocol.array_frame(@messages)
     end
