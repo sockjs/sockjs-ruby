77913d12968c34ecd84796393465679289767a8b
botanicus <stastny@101ideas.cz>
2012-01-03 12:26:57 +0000

    We don't have to support anything apart of Thin.

--- lib/sockjs/adapters/servers/rack.rb
+++ lib/sockjs/adapters/servers/rack.rb
@@ -62,42 +62,5 @@ module SockJS
         @env["rack.input"]
       end
     end
-
-    class Response < Response
-      def write_head(status = nil, headers = nil)
-        super(status, headers) do
-          # The actual implementation.
-        end
-      end
-
-      def write(data)
-        super() do
-          @body << data
-        end
-      end
-
-      def finish(data = nil)
-        super(data) do
-          @body = [@body] if @body.respond_to?(:bytesize)
-          [@status, @headers, @body]
-        end
-      end
-    end
-
-    class DelayedResponseBody
-      # Implementation: fibers? EM?
-
-      # response.write("data")
-      def each(&block)
-        # wait until finish
-        # block.call(data) if block
-      end
-
-      # this refactoring means we'll return [200, {}, []] in first write, not finish!
-
-      def finish
-        # done
-      end
-    end
   end
 end
