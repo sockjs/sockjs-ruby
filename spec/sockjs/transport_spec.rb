d409723010a75125e3fc4be9c764a616aad30e74
botanicus <stastny@101ideas.cz>
2012-01-04 22:37:33 +0000

    Some more specs ...

--- spec/sockjs/transport_spec.rb
+++ spec/sockjs/transport_spec.rb
@@ -91,4 +91,67 @@ describe SockJS::Transport do
       described_class.handler("/a/b/websocket").should eql(transport_class)
     end
   end
+
+  context "instance methods" do
+    describe "#initialize(connection, options)" do
+      # TODO
+    end
+
+    subject do
+      described_class.new(Object.new, Hash.new)
+    end
+
+    describe "#disabled?" do
+      it "should be false if the current class isn't in disabled_transports" do
+        subject.should_not be_disabled
+      end
+
+      it "should be false if the current class is in disabled_transports" do
+        subject.options[:disabled_transports] = [subject.class]
+        subject.should be_disabled
+      end
+    end
+
+    describe "#session_class" do
+      it "should be a valid class" do
+        subject.session_class.should be_kind_of(Class)
+      end
+    end
+
+    describe "#response_class" do
+      it "should be a valid class" do
+        subject.response_class.should be_kind_of(Class)
+      end
+    end
+
+    describe "#format_frame(payload)" do
+      it "should fail if payload is nil" do
+        -> { subject.format_frame(nil) }.should raise_error(TypeError)
+      end
+
+      it "should return payload followed by \\n otherwise" do
+        subject.format_frame("o").should eql("o\n")
+      end
+    end
+
+    describe "#send(data, *args)" do
+      # TODO
+    end
+
+    describe "#finish" do
+      # TODO
+    end
+
+    describe "#respond(request, status, options = Hash.new, &block)" do
+      # TODO
+    end
+
+    describe "#error(http_status, content_type, body)" do
+      # TODO
+    end
+
+    describe "#get_session(request, response, preamble = nil)" do
+      # TODO
+    end
+  end
 end
