fd30a844d85b2743c5a9da0da9ed1a42c63be498
botanicus <stastny@101ideas.cz>
2012-01-10 03:56:14 +0000

    Basic URL checks.

--- spec/sockjs/transports/websocket_spec.rb
+++ spec/sockjs/transports/websocket_spec.rb
@@ -7,4 +7,7 @@ require "sockjs"
 require "sockjs/transports/websocket"
 
 describe SockJS::Transports::WebSocket do
+  it_should_match_path  "server/session/websocket"
+  it_should_have_method "GET"
+  transport_handler_eql "a/b/websocket", "GET"
 end
