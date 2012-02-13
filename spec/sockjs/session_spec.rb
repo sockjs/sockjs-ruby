f5b4584e8a81b235948269b03ff43d18eae35850
botanicus <stastny@101ideas.cz>
2012-02-13 09:18:46 +0000

    Now working on specs for Session and SessionWitchCachedMessages.

---
+++ spec/sockjs/session_spec.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env bundle exec rspec
+# encoding: utf-8
+
+require "spec_helper"
+
+require "sockjs"
+require "sockjs/session"
+
+describe SockJS::Session do
+end
+
+describe SockJS::SessionWitchCachedMessages do
+end
