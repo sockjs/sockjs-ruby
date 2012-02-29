fd2635963013c7bc40b44a5c521a00fd0c9bf3a6
botanicus <stastny@101ideas.cz>
2012-02-29 15:32:14 +0000

    Added script for unpacking the tests.

---
+++ unpack_tests.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# Get list of all the tests in format for TODO.todo.
+
+VERSION = "0.2.1"
+
+tests = File.foreach("../protocol/sockjs-protocol-#{VERSION}.py").reduce(Hash.new) do |buffer, line|
+  if line.match(/class (\w+)\(Test\)/)
+    buffer[$1] = Array.new
+  elsif line.match(/def (\w+)/)
+    if buffer.keys.last
+      buffer[buffer.keys.last] << $1
+    end
+  end
+  
+  buffer
+end
+
+require "yaml"
+
+puts tests.to_yaml
