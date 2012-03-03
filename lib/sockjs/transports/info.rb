f531dbf9c88e144db7c71f5e2ec871c4234272c7
botanicus <stastny@101ideas.cz>
2012-03-03 11:06:37 +0100

    InfoTest.test_entropy passes

--- lib/sockjs/transports/info.rb
+++ lib/sockjs/transports/info.rb
@@ -7,12 +7,32 @@ module SockJS
 
     class Info < Transport
       # Settings.
-      self.prefix = /\/info$/
+      self.prefix = "info"
       self.method = "GET"
 
       # Handler.
       def handle(request)
-        # TODO: Continue here ...
+        response(request, 200) do |response|
+          response.set_content_type(:json)
+          response.set_access_control(request.origin)
+          response.set_allow_options_post
+          response.write(self.info.to_json)
+        end
+      end
+
+      def info
+        {
+            # websocket: "@options.websocket",
+            # origins: "@options.origins",
+            # cookie_needed: "not not @options.jsessionid",
+            entropy: self.entropy
+        }
+      end
+
+      def entropy
+        foo = -> { (rand * 256).round }
+        v = [foo.(), foo.(), foo.(), foo.()]
+        return v[0] + (v[1] * 256 ) + (v[2] * 256 * 256) + (v[3] * 256 * 256 * 256)
       end
     end
   end
