fe8abd535557c1feb8a256858f8cfbabd6a8268b
botanicus <stastny@101ideas.cz>
2012-03-17 22:58:32 +0000

    WSDebuggingMixin

--- lib/sockjs/transports/websocket.rb
+++ lib/sockjs/transports/websocket.rb
@@ -4,30 +4,6 @@ require_relative "./raw_websocket"
 
 module SockJS
   module Transports
-    module WSDebuggingMixin
-      def send_data(*args)
-        if args.length == 1
-          data = args.first
-        else
-          data = fix_buggy_input(*args)
-        end
-
-        if $DEBUG
-          puts "~> WS#send #{data.inspect} #{caller[0..2].map { |item| item.sub(Dir.pwd + "/lib/", "") }.inspect}"
-        else
-          puts "~> WS#send #{data.inspect}"
-        end
-
-        super(data)
-      end
-
-      def fix_buggy_input(*args)
-        data = 'c[3000,"Go away!"]'
-        puts "! Incorrect input: #{args.inspect}, changing to #{data} for now"
-        return data
-      end
-    end
-
     class WebSocket < Transport
       extend Forwardable
 
