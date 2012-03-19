afad899745f7c05839a2e05e2a1a9860fb7186f0
botanicus <stastny@101ideas.cz>
2012-03-19 23:07:23 +0000

    Thin hacks refactored, yippee!

--- lib/sockjs/thin.rb
+++ lib/sockjs/thin.rb
@@ -1,5 +1,9 @@
 # encoding: utf-8
 
+require "thin/request"
+require "thin/response"
+require "thin/connection"
+
 module Thin
   class Request
     HTTP_1_1 ||= "HTTP/1.1".freeze
@@ -8,27 +12,22 @@ module Thin
   class Response
     attr_writer :http_version
     def http_version
-      @http_version || Thin::Request::HTTP_1_1
+      @http_version || ::Thin::Request::HTTP_1_1
     end
 
-    include Module.new {
-      # Top header of the response,
-      # containing the status code and response headers.
-      def head
-        "HTTP/#{self.http_version} #{@status} #{HTTP_STATUS_CODES[@status.to_i]}\r\n#{headers_output}\r\n"
-      end
-    }
+    def head
+      "HTTP/#{self.http_version} #{@status} #{HTTP_STATUS_CODES[@status.to_i]}\r\n#{headers_output}\r\n"
+    end
   end
 
   class Connection
-    include Module.new {
-      # Called when all data was received and the request
-      # is ready to be processed.
-      def process
-        @response.http_version = @request.env[Thin::Request::HTTP_VERSION]
-        puts "~ HTTP version is #{@request.env[Thin::Request::HTTP_VERSION]}"
-        super
-      end
-    }
+    alias_method :_process, :process
+
+    # Called when all data was received and the request
+    # is ready to be processed.
+    def process
+      @response.http_version = @request.env[Thin::Request::HTTP_VERSION]
+      _process
+    end
   end
 end
