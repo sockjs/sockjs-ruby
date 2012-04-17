62712d6de40d662ba7572cd5579163145e8fb7ee
botanicus <stastny@101ideas.cz>
2012-04-17 09:14:41 +0800

    Link the original source.

--- lib/sockjs/thin.rb
+++ lib/sockjs/thin.rb
@@ -10,6 +10,7 @@ module Thin
 
     alias_method :_persistent?, :persistent?
 
+    # https://github.com/macournoyer/thin/blob/master/lib/thin/request.rb#L109-122
     def persistent?
       puts "~ Determining request persistency:"
 
